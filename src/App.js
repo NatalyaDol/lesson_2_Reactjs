95b5a4a3915697a7d1a3923743a3143dfe526e50
NatalyaDol <suhoikova@gmail.com>
2019-06-11 16:00:33 +0300

    Lesson 2 React.js is ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { developer, developerVanya } from './developer';
+
+
+
 class App extends React.Component {
   render() {
     return <div>
       <h1>Первое приложение на React</h1>
       <p>Hello React!</p>
+      <p>developerFirst</p>
     </div>
   }
 }
